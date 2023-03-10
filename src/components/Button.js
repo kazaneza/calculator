ed0ef183779c1bce436dc2794d493d70cb197ceb
kazaneza <kazanezagentil@gmail.com>
2023-03-10 23:15:03 +0100

    Added calculator buttons

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,17 +1,48 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function Button({ onClick, label }) {
+const Buttons = (props) => {
+  const { handleClick } = props;
   return (
-    <button type="button" onClick={onClick}>
-      {label}
-    </button>
+    <div className="calculator-wrapper">
+      <div className="calculator-numbers-cont">
+        <div className="calculator-numbers">
+          <button type="button" onClick={() => handleClick('AC')}>AC</button>
+          <button type="button" onClick={() => handleClick('+/-')}>+/-</button>
+          <button type="button" onClick={() => handleClick('%')}>%</button>
+        </div>
+        <div className="calculator-numbers">
+          <button type="button" onClick={() => handleClick('7')}>7</button>
+          <button type="button" onClick={() => handleClick('8')}>8</button>
+          <button type="button" onClick={() => handleClick('9')}>9</button>
+        </div>
+        <div className="calculator-numbers">
+          <button type="button" onClick={() => handleClick('4')}>4</button>
+          <button type="button" onClick={() => handleClick('5')}>5</button>
+          <button type="button" onClick={() => handleClick('6')}>6</button>
+        </div>
+        <div className="calculator-numbers">
+          <button type="button" onClick={() => handleClick('1')}>1</button>
+          <button type="button" onClick={() => handleClick('2')}>2</button>
+          <button type="button" onClick={() => handleClick('3')}>3</button>
+        </div>
+        <div className="calculator-numbers">
+          <button type="button" onClick={() => handleClick('0')}>0</button>
+          <button type="button" onClick={() => handleClick('.')}>.</button>
+        </div>
+      </div>
+      <div className="calculator-symbol-cont">
+        <button type="button" onClick={() => handleClick('÷')}>÷</button>
+        <button type="button" onClick={() => handleClick('x')}>×</button>
+        <button type="button" onClick={() => handleClick('-')}>-</button>
+        <button type="button" onClick={() => handleClick('+')}>+</button>
+        <button type="button" onClick={() => handleClick('=')}>=</button>
+      </div>
+    </div>
   );
-}
-
-Button.propTypes = {
-  onClick: PropTypes.func.isRequired,
-  label: PropTypes.string.isRequired,
 };
 
-export default Button;
+Buttons.propTypes = { handleClick: PropTypes.func };
+Buttons.defaultProps = { handleClick: '' };
+
+export default Buttons;
