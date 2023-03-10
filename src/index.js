adfb1ad559d3e155fa9b832d37982398babaf8a2
kazaneza <kazanezagentil@gmail.com>
2023-03-11 00:41:49 +0100

    solved linters error

--- src/index.js
+++ src/index.js
@@ -1,25 +1,10 @@
-import './App.css';
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-} from 'react-router-dom';
-import Calculator from './components/Calculator';
-import Home from './pages/Home';
-import Quotes from './pages/Quotes';
-import Navbar from './components/Navbar';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 
-function App() {
-  return (
-    <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/quotes" element={<Quotes />} />
-        <Route path="/Calculator" element={<Calculator />} />
-      </Routes>
-    </BrowserRouter>
-  );
-}
-
-export default App;
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
